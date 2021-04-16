8f1106206c7832513ef33d2579ecf22f8e4aea49
pedreviljoen <pedre87@gmail.com>
2021-04-16 19:13:39 +0200

    Added middleware to server

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
+
 # DB Dependency
 def get_db():
     db = SessionLocal()
